723b3ec80d8f22e3879138014d34d567399f9379
SL-Nick-Chen <nick.chen@softleader.com.tw>
2026-05-04 10:32:55 +0800

    init

---
+++ src/main/java/com/nick/mantis_jira_bridge/MantisJiraBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.nick.mantis_jira_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantisJiraBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantisJiraBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nick/mantis_jira_bridge/MantisJiraBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.mantis_jira_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantisJiraBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
